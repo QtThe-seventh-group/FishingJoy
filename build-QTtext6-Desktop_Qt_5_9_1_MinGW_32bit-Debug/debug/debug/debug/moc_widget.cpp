c69cb11023cf366367e4807639244d6d4fd3e4cb
Quinine2022 <108316290+Quinine2022@users.noreply.github.com>
2022-07-12 11:40:59 +0800

    Add files via upload

---
+++ build-QTtext6-Desktop_Qt_5_9_1_MinGW_32bit-Debug/debug/debug/debug/moc_widget.cpp
@@ -0,0 +1,152 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'widget.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../FishingJoy/QTtext6/widget.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'widget.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Widget_t {
+    QByteArrayData data[12];
+    char stringdata0[115];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Widget_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Widget_t qt_meta_stringdata_Widget = {
+    {
+QT_MOC_LITERAL(0, 0, 6), // "Widget"
+QT_MOC_LITERAL(1, 7, 10), // "startclick"
+QT_MOC_LITERAL(2, 18, 0), // ""
+QT_MOC_LITERAL(3, 19, 9), // "helpclick"
+QT_MOC_LITERAL(4, 29, 11), // "finishclick"
+QT_MOC_LITERAL(5, 41, 9), // "shopclick"
+QT_MOC_LITERAL(6, 51, 8), // "bugclick"
+QT_MOC_LITERAL(7, 60, 9), // "bugclick1"
+QT_MOC_LITERAL(8, 70, 9), // "bugclick2"
+QT_MOC_LITERAL(9, 80, 9), // "bugclick3"
+QT_MOC_LITERAL(10, 90, 12), // "improvelevel"
+QT_MOC_LITERAL(11, 103, 11) // "listenclick"
+
+    },
+    "Widget\0startclick\0\0helpclick\0finishclick\0"
+    "shopclick\0bugclick\0bugclick1\0bugclick2\0"
+    "bugclick3\0improvelevel\0listenclick"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Widget[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      10,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   64,    2, 0x0a /* Public */,
+       3,    0,   65,    2, 0x0a /* Public */,
+       4,    0,   66,    2, 0x0a /* Public */,
+       5,    0,   67,    2, 0x0a /* Public */,
+       6,    0,   68,    2, 0x0a /* Public */,
+       7,    0,   69,    2, 0x0a /* Public */,
+       8,    0,   70,    2, 0x0a /* Public */,
+       9,    0,   71,    2, 0x0a /* Public */,
+      10,    0,   72,    2, 0x0a /* Public */,
+      11,    0,   73,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Widget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Widget *_t = static_cast<Widget *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->startclick(); break;
+        case 1: _t->helpclick(); break;
+        case 2: _t->finishclick(); break;
+        case 3: _t->shopclick(); break;
+        case 4: _t->bugclick(); break;
+        case 5: _t->bugclick1(); break;
+        case 6: _t->bugclick2(); break;
+        case 7: _t->bugclick3(); break;
+        case 8: _t->improvelevel(); break;
+        case 9: _t->listenclick(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject Widget::staticMetaObject = {
+    { &QWidget::staticMetaObject, qt_meta_stringdata_Widget.data,
+      qt_meta_data_Widget,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *Widget::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Widget::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Widget.stringdata0))
+        return static_cast<void*>(const_cast< Widget*>(this));
+    return QWidget::qt_metacast(_clname);
+}
+
+int Widget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 10)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 10;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 10)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 10;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
